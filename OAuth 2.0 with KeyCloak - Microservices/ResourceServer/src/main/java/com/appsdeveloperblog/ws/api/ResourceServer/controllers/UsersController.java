59e1641a7f0e138976600f73adc1a60218f5d4bf
Oleh Maliarchuk <oleh_maliarchuk@epam.com>
2023-01-22 22:42:33 +0200

    Added load balancing

--- OAuth 2.0 with KeyCloak - Microservices/ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
+++ OAuth 2.0 with KeyCloak - Microservices/ResourceServer/src/main/java/com/appsdeveloperblog/ws/api/ResourceServer/controllers/UsersController.java
@@ -1,6 +1,8 @@
 package com.appsdeveloperblog.ws.api.ResourceServer.controllers;
 
 import com.appsdeveloperblog.ws.api.ResourceServer.response.UserRest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,13 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "Working...";
+        return "Working on port: " + environment.getProperty("local.server.port");
     }
 
     @PreAuthorize("#id == #jwt.subject")
-    //@Secured("ROLE_developer")
     @DeleteMapping(path = "/{id}")
     public String deleteUser(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
         return "Deleted user with id " + id + " and JWT subject " + jwt.getSubject();
