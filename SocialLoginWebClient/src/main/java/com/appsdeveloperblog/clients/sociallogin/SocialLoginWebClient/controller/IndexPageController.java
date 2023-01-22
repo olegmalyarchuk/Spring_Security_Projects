bfd1efe2e7058d29bef33bde8c728759e2168657
Oleh Maliarchuk <oleh_maliarchuk@epam.com>
2023-01-23 01:09:56 +0200

    Added Controller&Simple pages

---
+++ SocialLoginWebClient/src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/controller/IndexPageController.java
@@ -0,0 +1,14 @@
+package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexPageController {
+
+    @GetMapping("/")
+    public String displayIndexPage(Model model) {
+        return "index";
+    }
+}
---
+++ SocialLoginWebClient/src/main/java/com/appsdeveloperblog/clients/sociallogin/SocialLoginWebClient/security/WebSecurity.java
@@ -0,0 +1,37 @@
+package com.appsdeveloperblog.clients.sociallogin.SocialLoginWebClient.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> {
+                            try {
+                                authz
+                                        .requestMatchers("/").permitAll()
+                                        .anyRequest().authenticated()
+                                        .and()
+                                        .oauth2Login();
+
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
