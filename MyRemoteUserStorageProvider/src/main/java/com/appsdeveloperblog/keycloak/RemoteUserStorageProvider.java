189ecfd968b3a871301a6925fc65be0078bffacc
Oleh Maliarchuk <oleh_maliarchuk@epam.com>
2023-01-23 09:00:53 +0200

    Added RemoteUserStorage provider&factory

---
+++ MyRemoteUserStorageProvider/src/main/java/com/appsdeveloperblog/keycloak/RemoteUserStorageProvider.java
@@ -0,0 +1,55 @@
+package com.appsdeveloperblog.keycloak;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.models.*;
+import org.keycloak.provider.Provider;
+import org.keycloak.provider.ProviderFactory;
+import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.user.UserLookupProvider;
+
+public class RemoteUserStorageProvider implements UserStorageProvider, UserLookupProvider, CredentialInputValidator {
+    private KeycloakSession keycloakSession;
+    private ComponentModel componentModel;
+
+    public RemoteUserStorageProvider(KeycloakSession keycloakSession, ComponentModel componentModel) {
+        this.keycloakSession = keycloakSession;
+        this.componentModel = componentModel;
+    }
+
+    @Override
+    public boolean supportsCredentialType(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean isConfiguredFor(RealmModel realmModel, UserModel userModel, String s) {
+        return false;
+    }
+
+    @Override
+    public boolean isValid(RealmModel realmModel, UserModel userModel, CredentialInput credentialInput) {
+        return false;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public UserModel getUserById(String s, RealmModel realmModel) {
+        return null;
+    }
+
+    @Override
+    public UserModel getUserByUsername(String s, RealmModel realmModel) {
+        return null;
+    }
+
+    @Override
+    public UserModel getUserByEmail(String s, RealmModel realmModel) {
+        return null;
+    }
+}
