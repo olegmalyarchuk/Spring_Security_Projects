b72114be446bb26d348690e3bef230c10091fe24
Oleh Maliarchuk <oleh_maliarchuk@epam.com>
2023-01-23 09:56:56 +0200

    Implemented getUserByUsername()

--- Keycloak Server SPI/MyRemoteUserStorageProvider/src/main/java/com/appsdeveloperblog/keycloak/provider/RemoteUserStorageProvider.java
+++ Keycloak Server SPI/MyRemoteUserStorageProvider/src/main/java/com/appsdeveloperblog/keycloak/provider/RemoteUserStorageProvider.java
@@ -1,19 +1,27 @@
 package com.appsdeveloperblog.keycloak.provider;
 
+import com.appsdeveloperblog.keycloak.response.User;
+import com.appsdeveloperblog.keycloak.service.UsersApiService;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputValidator;
 import org.keycloak.models.*;
 import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.adapter.AbstractUserAdapter;
 import org.keycloak.storage.user.UserLookupProvider;
 
 public class RemoteUserStorageProvider implements UserStorageProvider, UserLookupProvider, CredentialInputValidator {
     private KeycloakSession keycloakSession;
     private ComponentModel componentModel;
+    private UsersApiService usersApiService;
 
-    public RemoteUserStorageProvider(KeycloakSession keycloakSession, ComponentModel componentModel) {
+    public RemoteUserStorageProvider(KeycloakSession keycloakSession,
+                                     ComponentModel componentModel,
+                                     UsersApiService usersApiService) {
         this.keycloakSession = keycloakSession;
         this.componentModel = componentModel;
+        this.usersApiService = usersApiService;
+
     }
 
     @Override
@@ -42,8 +50,27 @@ public class RemoteUserStorageProvider implements UserStorageProvider, UserLooku
     }
 
     @Override
-    public UserModel getUserByUsername(String s, RealmModel realmModel) {
-        return null;
+    public UserModel getUserByUsername(String username, RealmModel realmModel) {
+
+        UserModel returnValue = null;
+
+        User user = usersApiService.getUserDetails(username);
+
+        if (user != null) {
+            returnValue = createUserModel(username, realmModel);
+        }
+
+
+        return returnValue;
+    }
+
+    private UserModel createUserModel(String username, RealmModel realmModel) {
+        return new AbstractUserAdapter(keycloakSession, realmModel, componentModel) {
+            @Override
+            public String getUsername() {
+                return username;
+            }
+        };
     }
 
     @Override
--- Keycloak Server SPI/MyRemoteUserStorageProvider/src/main/java/com/appsdeveloperblog/keycloak/provider/RemoteUserStorageProviderFactory.java
+++ Keycloak Server SPI/MyRemoteUserStorageProvider/src/main/java/com/appsdeveloperblog/keycloak/provider/RemoteUserStorageProviderFactory.java
@@ -1,20 +1,32 @@
 package com.appsdeveloperblog.keycloak.provider;
 
+import com.appsdeveloperblog.keycloak.service.UsersApiService;
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.storage.UserStorageProviderFactory;
 
 public class RemoteUserStorageProviderFactory implements UserStorageProviderFactory<RemoteUserStorageProvider> {
 
-    public static final String PROVIDER_NAME="my-remote-mysql-user-storage-provider";
+    public static final String PROVIDER_NAME = "my-remote-mysql-user-storage-provider";
 
     @Override
     public RemoteUserStorageProvider create(KeycloakSession keycloakSession, ComponentModel componentModel) {
-        return new RemoteUserStorageProvider(keycloakSession, componentModel);
+        return new RemoteUserStorageProvider(keycloakSession, componentModel, buildHttpClient("http://localhost:8099"));
     }
 
     @Override
     public String getId() {
         return PROVIDER_NAME;
+    }
+
+    private UsersApiService buildHttpClient(String uri) {
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target(uri);
+
+        return target.proxyBuilder(UsersApiService.class)
+                .classloader(UsersApiService.class.getClassLoader()).build();
     }
 }
