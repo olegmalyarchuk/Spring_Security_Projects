449da74c2b31046b85a692c9a4baa24580c51d7a
Oleh Maliarchuk <oleh_maliarchuk@epam.com>
2023-01-23 11:12:33 +0200

    SpringAuthorizationServer Scratch Project

---
+++ Spring Authorization Server/SpringAuthorizationServer/src/test/java/com/appsdeveloperblog/photoapp/oauthserver/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.photoapp.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
